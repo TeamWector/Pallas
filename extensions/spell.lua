35ec9815ca9c601ce39411ad428d460af0c7c411
Ian <github@wector.info>
2022-08-14 19:27:33 +0200

    fix: added IsActive to the spell can use check
    To avoid it casting spells a million times before the cast actually finishes

--- extensions/spell.lua
+++ extensions/spell.lua
@@ -1,4 +1,4 @@
 function WoWSpell:CanUse(target)
   if not target then target = wector.Game.ActivePlayer.ToUnit end
-  return self.IsReady and self:IsUsable() and self:InRange(target) and (self.CastTime == 0 or not wector.Game.ActivePlayer:IsMoving())
+  return self.IsReady and not self.IsActive and self:IsUsable() and self:InRange(target) and (self.CastTime == 0 or not wector.Game.ActivePlayer:IsMoving())
 end
