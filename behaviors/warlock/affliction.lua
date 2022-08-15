f16058ed16fb8f4d9eb96acf7e83503d3bab183a
Neer <legitting@gmail.com>
2022-08-15 20:28:33 +0200

    Add lifetap feature and more stability to drain tank rotation overall

--- behaviors/warlock/affliction.lua
+++ behaviors/warlock/affliction.lua
@@ -48,6 +48,29 @@ function WarlockAfflListener:UNIT_SPELLCAST_SUCCEEDED(unitTarget, _, spellID)
   end
 end
 
+local function LifetapValue()
+  local spellid = spells.LifeTap.Id
+
+  if spellid == 1454 then
+    return 30
+  elseif spellid == 1455 then
+    return 90
+  elseif spellid == 1456 then
+    return 168
+  elseif spellid == 11678 then
+    return 264
+  elseif spellid == 11688 then
+    return 372
+  elseif spellid == 11689 then
+    return 516
+  elseif spellid == 27222 then
+    return 698
+  end
+
+  -- why tho?
+  return 0
+end
+
 local function WarlockAfflictionCombat()
   -- Threshold % on me for LifeTap
   local HPThresh = Settings.WarlockAfflLifeTapPercent
@@ -55,13 +78,17 @@ local function WarlockAfflictionCombat()
   local SpellThresh = Settings.WarlockAfflWandExecutePercent
 
   -- buff up
-
-  -- fix for broken IsKnown
   local spellDemonBuff = spells.DemonArmor.Slot >= 0 and spells.DemonArmor or spells.DemonSkin
-  --local spellDemonBuff = spells.DemonArmor.IsKnown and spells.DemonArmor or spells.DemonSkin
 
-  -- Demon Armor/Skin
-  if not Me:HasVisibleAura(spellDemonBuff.Id) and spellDemonBuff:CastEx(Me) then return end
+  -- Always make sure we are out of combat for buffs and summons and shit
+  if not Me.InCombat then
+    -- Demon Armor/Skin
+    if not Me:HasVisibleAura(spellDemonBuff.Id) and spellDemonBuff:CastEx(Me) then return end
+  end
+
+  -- Lifetap anytime to gain value outside of combat also
+  -- Life Tap if our health is more than what we choose and our mana deficit is more than what lifetap will return
+  if Me.HealthPct >= HPThresh and not Me.IsCastingOrChanneling and (Me.PowerMax - Me.Power >= LifetapValue()) and spells.LifeTap:CastEx(Me) then return end
 
   -- Make sure we have a target before continuing
   local target = Combat.BestTarget
@@ -73,23 +100,17 @@ local function WarlockAfflictionCombat()
     if not Me.Pet.Target or Me.Pet.Target ~= Me.Target then
       Me:PetAttack(target)
     end
-
-    -- set follow if no target
-    if not target and Me.Pet.Target then
-      Me:PetFollow()
-    end
   end
+
+  -- Wand Finisher if toggled and below health percentage
+  if target:GetHealthPercent() <= SpellThresh and Settings.WarlockAfflWandFinish then
+    if not spells.Shoot.IsAutoRepeat and spells.Shoot:CastEx(target) then return end
+  end
+
+  -- if we are channeling or casting we should return so we dont cancel drain life/soul
+  if Me.IsCastingOrChanneling then return end
 
   if Me:HasBuff("Shadow Trance") and spells.ShadowBolt:CastEx(target) then return end
-
-  -- Wand finisher, convert to percentage when that is implemented
-  if target:GetHealthPercent() <= SpellThresh then
-    if not spells.Shoot.IsAutoRepeat and spells.Shoot:CastEx(target) then return end
-
-    return
-  end
-
-  if Me.IsCastingOrChanneling then return end
 
   -- Curruption
   local shouldCorruption = (wector.Game.Time - corruptionFix) > 0
@@ -98,18 +119,15 @@ local function WarlockAfflictionCombat()
   -- Curse of Agony
   if not target:HasDebuffByMe("Curse of Agony") and spells.CurseOfAgony:CastEx(target) then return end
 
-  -- Drain Life
-  if Me.PowerPct > 70 and spells.DrainLife:CastEx(target) then return end
-
-  -- Life Tap
-  if Me.HealthPct >= HPThresh and Me.PowerPct < 50 and not Me.IsCastingOrChanneling then
-    if spells.LifeTap:CastEx(Me) then return end
-  end
+  -- Drain Life if our mana is above a percentage
+  if Me.PowerPct > 60 and spells.DrainLife:CastEx(target) then return end
 
   -- Wand
   -- Only if not fully debuffed
   if not target:HasDebuffByMe("Curse of Agony") or not target:HasDebuffByMe("Corruption") then return end
-  if not spells.Shoot.IsAutoRepeat and spells.Shoot:CastEx(target) then return end
+
+  -- Use wand if mana below drain life mana threshold
+  if not spells.Shoot.IsAutoRepeat and Me.PowerPct <= 60 and spells.Shoot:CastEx(target) then return end
 end
 
 local behaviors = {
