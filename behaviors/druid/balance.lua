9f30bce61f0ebb28389cbb581b291c95c8683dcc
Ian <github@wector.info>
2022-08-23 11:41:08 +0200

    druid/balance: added basic leveling behavior

--- behaviors/druid/balance.lua
+++ behaviors/druid/balance.lua
@@ -1,12 +1,42 @@
+local spells = {
+  Starfire = WoWSpell("Starfire"),
+  Wrath = WoWSpell("Wrath"),
+  InsectSwarm = WoWSpell("Insect Swarm"),
+  Moonfire = WoWSpell("Moonfire"),
+
+  MarkOfTheWild = WoWSpell("Mark of the Wild"),
+  Thorns = WoWSpell("Thorns"),
+}
+
 local function BalanceHeal()
 
 end
 
 local function BalanceCombat()
+  if Me.IsCastingOrChanneling then return end
 
+  -- to many forms!
+  local form = Me.ShapeshiftForm
+  if form == ShapeshiftForm.Cat or form == ShapeshiftForm.Bear or form == ShapeshiftForm.Aqua or form == ShapeshiftForm.DireBear or form == ShapeshiftForm.EpicFlightForm or form == ShapeshiftForm.Travel then return end
+
+  if not Me.InCombat then
+    if not Me:HasVisibleAura("Mark of the Wild") and spells.MarkOfTheWild:CastEx(Me) then return end
+    if not Me:HasVisibleAura("Thorns") and spells.Thorns:CastEx(Me) then return end
+  end
+
+  local target = Combat.BestTarget
+  if not target then return end
+
+  if not target:HasVisibleAura("Insect Swarm") and spells.InsectSwarm:CastEx(target) then return end
+  if Me.PowerPct > 20 and not target:HasVisibleAura("Moonfire") and spells.Moonfire:CastEx(target) then return end
+
+  if Me:IsMoving() then return end
+  if spells.Starfire:CastEx(target) then return end
 end
 
 return {
-  [BehaviorType.Heal] = BalanceHeal,
-  [BehaviorType.Combat] = BalanceCombat
+  Behaviors = {
+    [BehaviorType.Heal] = BalanceHeal,
+    [BehaviorType.Combat] = BalanceCombat
+  }
 }
