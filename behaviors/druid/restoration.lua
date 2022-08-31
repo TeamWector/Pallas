e12e06efe788a23883b6911617f9f26ed60fabfb
Ian <github@wector.info>
2022-08-31 22:51:04 +0200

    updated resto druid behavior

--- behaviors/druid/restoration.lua
+++ behaviors/druid/restoration.lua
@@ -3,6 +3,8 @@ local spells = {
   HealingTouch = WoWSpell("Healing Touch"),
   Regrowth = WoWSpell("Regrowth"),
   Swiftmend = WoWSpell("Swiftmend"),
+  Lifebloom = WoWSpell("Lifebloom"),
+  WildGrowth = WoWSpell("Wild Growth"),
 
   MarkOfTheWild = WoWSpell("Mark of the Wild"),
   GiftOfTheWild = WoWSpell("Gift of the Wild"),
@@ -18,8 +20,12 @@ local function DruidRestoHeal()
     local u = v.Unit
     local prio = v.Priority
 
-    if prio > 25 and u:HasBuffByMe("Rejuvenation") and spells.Swiftmend:CastEx(u) then return end
-    if prio > 1 and not u:HasBuffByMe("Rejuvenation") and spells.Rejuvenation:CastEx(u) then return end
+    if prio > 40 and spells.Regrowth:CastEx(u) then return end
+    if prio > 25 and u:HasBuffByMe("Rejuvenation") and spells.Swiftmend:CastEx(u, SpellCastExFlags.NoUsable) then return end
+    if Me:HasVisibleAura("Clearcasting") and spells.Regrowth:CastEx(u) then return end
+    if not u:HasBuffByMe("Lifebloom") and spells.Lifebloom:CastEx(u) then return end
+    if prio > 30 and spells.WildGrowth:CastEx(u) then return end
+    if not u:HasBuffByMe("Rejuvenation") and spells.Rejuvenation:CastEx(u) then return end
   end
 end
 
