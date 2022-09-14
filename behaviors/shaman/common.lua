edc71bd29d3ccab2f438321454d3ca835310b897
Neer <legitting@gmail.com>
2022-09-14 19:56:00 +0200

    add shaman interrupt and ghost wolf

--- behaviors/shaman/common.lua
+++ behaviors/shaman/common.lua
@@ -8,26 +8,70 @@ commonShaman.widgets = {
         default = true
     },
     {
+        type = "slider",
+        uid = "InterruptTime",
+        text = "Interrupt Time (MS)",
+        default = 500,
+        min = 0,
+        max = 2000
+    },
+    {
         type = "checkbox",
         uid = "Ghostwolf",
         text = "Auto Ghostwolf",
         default = true
     },
+    {
+        type = "slider",
+        uid = "GhostwolfTime",
+        text = "Ghostwolf Timer (MS)",
+        default = 1500,
+        min = 0,
+        max = 5000
+    },
 }
+
+ShamanListener = wector.FrameScript:CreateListener()
+ShamanListener:RegisterEvent('PLAYER_STARTED_MOVING')
+ShamanListener:RegisterEvent('PLAYER_STOPPED_MOVING')
+
+local start = 0
+function ShamanListener:PLAYER_STARTED_MOVING()
+    start = wector.Game.Time
+end
+
+function ShamanListener:PLAYER_STOPPED_MOVING()
+    start = 0
+end
 
 function commonShaman:Interrupt()
     if Settings.ShamanInterrupt then
         local units = wector.Game.Units
         for _, u in pairs(units) do
             if u:InCombatWithMe() and u.IsCastingOrChanneling then
-                if Spell.WindShear:CastEx(u) then return end
+                local cast = u.CurrentCast
+                local timeLeft
+
+                if cast then
+                    timeLeft = cast.CastEnd - wector.Game.Time
+                else
+                    timeLeft = 0
+                end
+
+                if (timeLeft <= Settings.InterruptTime or u.IsChanneling) and Spell.WindShear:CastEx(u) then return end
             end
         end
     end
 end
 
 function commonShaman:Ghostwolf()
-    if Settings.Ghostwolf and Me:IsMoving() then
+    local timespentmoving = 0
+
+    if start ~= 0 then
+        timespentmoving = wector.Game.Time - start
+    end
+
+    if Settings.Ghostwolf and timespentmoving > Settings.GhostwolfTime and not Me.InCombat then
         if not Me:HasVisibleAura(Spell.GhostWolf.Name) and Spell.GhostWolf:CastEx(Me) then return end
     end
 end
