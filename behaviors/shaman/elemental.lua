d7f7238e8c1e7f8fd36c3b4a234b9b9dfc42d1b3
Neer <legitting@gmail.com>
2022-09-14 16:34:47 +0200

    add basic enhance and autoloot extra

--- behaviors/shaman/elemental.lua
+++ behaviors/shaman/elemental.lua
@@ -1,1 +1,23 @@
-return {}
+local options = {
+    -- The sub menu name
+    Name = "Shaman (Ele)",
+
+    -- widgets
+    Widgets = {
+        {
+            type = "checkbox",
+            uid = "placeholder",
+            text = "placeholder",
+            default = false
+        },
+    }
+}
+
+local function ShamanElementalCombat()
+end
+
+local behaviors = {
+    [BehaviorType.Combat] = ShamanElementalCombat
+}
+
+return { Options = options, Behaviors = behaviors }
