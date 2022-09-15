b425b73cff0ef533a3f0fc51f237c67180aaa6d2
Ian <github@wector.info>
2022-09-15 02:47:41 +0200

    druid/balance: update to use Spell

--- behaviors/druid/balance.lua
+++ behaviors/druid/balance.lua
@@ -1,13 +1,3 @@
-local spells = {
-  Starfire = WoWSpell("Starfire"),
-  Wrath = WoWSpell("Wrath"),
-  InsectSwarm = WoWSpell("Insect Swarm"),
-  Moonfire = WoWSpell("Moonfire"),
-
-  MarkOfTheWild = WoWSpell("Mark of the Wild"),
-  Thorns = WoWSpell("Thorns"),
-}
-
 local function BalanceHeal()
 
 end
@@ -21,20 +11,20 @@ local function BalanceCombat()
   if form == ShapeshiftForm.Cat or form == ShapeshiftForm.Bear or form == ShapeshiftForm.Aqua or form == ShapeshiftForm.DireBear or form == ShapeshiftForm.EpicFlightForm or form == ShapeshiftForm.Travel then return end
 
   if not Me.InCombat then
-    if not Me:HasVisibleAura("Mark of the Wild") and spells.MarkOfTheWild:CastEx(Me) then return end
-    if not Me:HasVisibleAura("Thorns") and spells.Thorns:CastEx(Me) then return end
+    if not Me:HasVisibleAura("Mark of the Wild") and Spell.MarkOfTheWild:CastEx(Me) then return end
+    if not Me:HasVisibleAura("Thorns") and Spell.Thorns:CastEx(Me) then return end
   end
 
   local target = Combat.BestTarget
   if not target then return end
 
   if target:TimeToDeath() > 5 then
-    if Me:IsFacing(target) and not target:HasVisibleAura("Insect Swarm") and spells.InsectSwarm:CastEx(target) then return end
-    if Me:IsFacing(target) and not target:HasVisibleAura("Moonfire") and spells.Moonfire:CastEx(target) then return end
+    if Me:IsFacing(target) and not target:HasVisibleAura("Insect Swarm") and Spell.InsectSwarm:CastEx(target) then return end
+    if Me:IsFacing(target) and not target:HasVisibleAura("Moonfire") and Spell.Moonfire:CastEx(target) then return end
   end
 
   if Me:IsMoving() then return end
-  if Me:IsFacing(target) and spells.Starfire:CastEx(target) then return end
+  if Me:IsFacing(target) and Spell.Starfire:CastEx(target) then return end
 end
 
 return {
